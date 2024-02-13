349f5e98c1cd2cb871cada772c820e148dac0ac6
Shivansh Dubey <96559712+Lazy18@users.noreply.github.com>
2024-02-13 22:06:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+"use strict"; // treat all code as newer version
+
+// data types in javascript
+
+let name = "Shivansh"
+var rollno = 20012001000
+let isloggedIn = false
+
+// number
+// bigint
+// null
+// undefined
+// String
+// Boolean
+
+console.log(typeof name);
