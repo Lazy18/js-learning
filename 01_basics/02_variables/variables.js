13682a11ea765e9add1a64e4b4e4247cff730af0
Shivansh Dubey <96559712+Lazy18@users.noreply.github.com>
2024-02-13 21:48:23 +0000

    variables

---
+++ 01_basics/02_variables/variables.js
@@ -0,0 +1,19 @@
+const account_Id = 141245
+let account_Email =  "shivansh@gmail.com"
+var accounnt_password = "12345"
+account_city = "jaipur"
+
+// account_Id = 3 ( not allowed )
+
+
+
+
+account_Email = "help@gmail.com"
+accounnt_password = "whyuwannaknow"
+account_city = "bengaluru"
+
+
+
+console.log(account_Id);
+
+console.table([account_Id,account_Email,accounnt_password,account_city])
