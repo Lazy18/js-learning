7e913b41921e39a424840d30a06309bdb0ff1fb7
Shivansh Dubey <96559712+Lazy18@users.noreply.github.com>
2024-02-13 22:24:31 +0000

    conversion

---
+++ 01_basics/converstionOperation.js
@@ -0,0 +1,24 @@
+let score = "18"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+
+console.log(valueInNumber);
+
+
+// 18 >= 18
+// 18abc >= NaN
+// true >= 1 , false >= 0
+// for boolean "" is false "hello" is true
+
+
+let someNumber = 23
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
