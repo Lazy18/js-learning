572381307c1397daa71a5c1e86299a65cd22d07f
Shivansh Dubey <96559712+Lazy18@users.noreply.github.com>
2024-02-13 21:52:55 +0000

    variables study

--- 01_basics/02_variables/variables.js
+++ 01_basics/02_variables/variables.js
@@ -2,6 +2,9 @@ const account_Id = 141245
 let account_Email =  "shivansh@gmail.com"
 var accounnt_password = "12345"
 account_city = "jaipur"
+let accountname
+
+
 
 // account_Id = 3 ( not allowed )
 
@@ -14,6 +17,7 @@ account_city = "bengaluru"
 
 
 
+
 console.log(account_Id);
 
-console.table([account_Id,account_Email,accounnt_password,account_city])
+console.table([account_Id,account_Email,accounnt_password,account_city,accountname])
